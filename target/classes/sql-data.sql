9968b602d5665d301a29b936db7b20f2ae844281
ben-noreika <66423091+noreb001@users.noreply.github.com>
2020-11-27 13:53:37 +0000

    added git ignore

--- target/classes/sql-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `IMS`.`customer` (`first_name`, `last_name`) VALUES ('jordan', 'harrison');
-
-INSERT INTO `IMS`.`product` (`name`, `model`, `stock`, `price`) VALUES ('hoover', 'hkfoor','10','20');
-
-INSERT INTO `IMS`.`orders` (`customer_id`, `order_date`, `total`) VALUES ( '1','2020-10-10', '0');
-
-INSERT INTO `IMS`.`orders_items` (`order_id`, `item_id`, `quantity`) VALUES ('1', '1','20');
